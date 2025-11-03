ea5e8a5abbca03ef45c0d3a43e2846f02159dca4
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-03 11:37:01 +0900

    feat: dtto 전환

--- backend/src/main/java/com/back/domain/post/comment/dto/CommentDto.kt
+++ backend/src/main/java/com/back/domain/post/comment/dto/CommentDto.kt
@@ -7,13 +7,13 @@ import java.time.LocalDateTime
 //코틀린을 자바로 바꿨다가 자바를 바이트 코드로 바꾸라는 의미
 //코틀린으로 100프로 다 전환하기 전까지는 해당 어노테이션 두는 게 좋다.
 data class CommentDto private constructor(
-    val id: Long?,
-    val createDate: LocalDateTime?,
-    val modifyDate: LocalDateTime?,
-    val content: String?,
-    val authorId: Long?,
-    val authorName: String?,
-    val postId: Long?
+    val id: Long,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val content: String,
+    val authorId: Long,
+    val authorName: String,
+    val postId: Long
 ) {
     constructor(comment: Comment) : this(
         comment.id,
